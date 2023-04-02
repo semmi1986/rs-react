442f4d4d32bce3b7ca80d271882656327e874ba2
Semen Samosiuk <samasiuk1986@gmail.com>
2023-04-02 13:42:43 +0300

    fix: class component Cards

--- react-component/src/page/Cards.tsx
+++ react-component/src/page/Cards.tsx
@@ -1,31 +1,25 @@
 import { Wrap } from '@chakra-ui/react';
-import { Component } from 'react';
-
+import { useState } from 'react';
 import product from '../assets/api.json';
 import CardItem from '../components/CardItem';
-import { DataState } from '../types';
 
-export default class Cards extends Component<unknown, { items: DataState[] }> {
-  constructor(props: unknown) {
-    super(props);
-    this.state = { items: product.products };
-  }
+function Cards() {
+  const [items, setItems] = useState(product.products);
 
-  render() {
-    // console.log(this.state.items);
-    return (
-      <Wrap>
-        {this.state.items.map((item) => (
-          <CardItem
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            thumbnail={item.thumbnail}
-            description={item.description}
-            price={item.price}
-          />
-        ))}
-      </Wrap>
-    );
-  }
+  return (
+    <Wrap>
+      {items.map((item) => (
+        <CardItem
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          thumbnail={item.thumbnail}
+          description={item.description}
+          price={item.price}
+        />
+      ))}
+    </Wrap>
+  );
 }
+
+export default Cards;
