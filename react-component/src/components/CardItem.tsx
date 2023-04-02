a0f2016e8256664fd8e194157e16a7b4f29e8285
Semen Samosiuk <samasiuk1986@gmail.com>
2023-04-02 13:52:22 +0300

    fix: class component CardItem

--- react-component/src/components/CardItem.tsx
+++ react-component/src/components/CardItem.tsx
@@ -11,52 +11,50 @@ import {
   WrapItem,
   Box,
 } from '@chakra-ui/react';
-import React, { Component } from 'react';
 import { DataState } from '../types';
 
-export default class CardItem extends Component<DataState> {
-  render() {
-    const { title, thumbnail, description, price } = this.props;
-    return (
-      <div className="cards" data-testid="cards">
-        <WrapItem>
-          <Card w="sm" h="500px" m="10px" bg="#3182ce61">
-            <CardBody>
-              <Box display="flex" alignItems="center" justifyContent="center">
-                <Image
-                  src={thumbnail}
-                  alt="product image"
-                  borderRadius="lg"
-                  boxSize="200px"
-                  mb={2}
-                />
-              </Box>
-              <Stack
-                spacing="3"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Heading size="md">{title}</Heading>
-                <Text fontSize="1rem">{description}</Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-            <CardFooter
+function CardItem({ title, thumbnail, description, price }: DataState) {
+  return (
+    <div className="cards" data-testid="cards">
+      <WrapItem>
+        <Card w="sm" h="500px" m="10px" bg="#3182ce61">
+          <CardBody>
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <Image
+                src={thumbnail}
+                alt="product image"
+                borderRadius="lg"
+                boxSize="200px"
+                mb={2}
+              />
+            </Box>
+            <Stack
+              spacing="3"
               display="flex"
               alignItems="center"
-              justifyContent="space-between"
+              justifyContent="center"
             >
-              <Button variant="solid" colorScheme="blue">
-                Buy now
-              </Button>
-              <Text color="rgba(228, 76, 76, 0.976)" fontSize="2xl">
-                ${price}
-              </Text>
-            </CardFooter>
-          </Card>
-        </WrapItem>
-      </div>
-    );
-  }
+              <Heading size="md">{title}</Heading>
+              <Text fontSize="1rem">{description}</Text>
+            </Stack>
+          </CardBody>
+          <Divider />
+          <CardFooter
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Button variant="solid" colorScheme="blue">
+              Buy now
+            </Button>
+            <Text color="rgba(228, 76, 76, 0.976)" fontSize="2xl">
+              ${price}
+            </Text>
+          </CardFooter>
+        </Card>
+      </WrapItem>
+    </div>
+  );
 }
+
+export default CardItem;
